bef91adf93723012e16cff165fe44872ec5aec8e
abrown5421 <augustashtonbrown@gmail.com>
2026-01-07 15:25:31 -0500

    adding in an address field to the boilerplate user feature so that we can keep track of billing and mailing addresses

--- apps/api/src/entities/user/user.model.ts
+++ apps/api/src/entities/user/user.model.ts
@@ -1,11 +1,33 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+type Address = {
+  addrLine1: string,
+  addrLine2?: string,
+  addrCity: string,
+  addrState: string,
+  addrZip: number
+}
+
+const AddressSchema = new Schema<Address>(
+  {
+    addrLine1: { type: String, required: true },
+    addrLine2: { type: String },
+    addrCity:  { type: String, required: true },
+    addrState: { type: String, required: true },
+    addrZip:   { type: Number, required: true },
+  },
+  { _id: false } 
+);
+
 export interface IUser extends Document {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
   type: 'user' | 'editor' | 'admin';
+  mailingAddress: Address;
+  billingAddress: Address;
+  sameAddress: boolean;
   profileImage?: string;
   createdAt: Date;
   updatedAt: Date;
@@ -18,6 +40,9 @@ const UserSchema: Schema<IUser> = new Schema(
     email:     { type: String, required: true, unique: true },
     password:  { type: String, required: true },
     type:      { type: String, enum: ['user','editor','admin'], default: 'user' },
+    mailingAddress: { type: AddressSchema },
+    billingAddress: { type: AddressSchema },
+    sameAddress: { type: Boolean },
     profileImage: { type: String },
   },
   { timestamps: true }
